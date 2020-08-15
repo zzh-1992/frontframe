7192bfb10ab108b7df99340f98ee0a619d148775
ZhangZhihuang <ezzh1992@icloud.com>
2020-08-15 17:03:14 +0800

    添加作者

--- frontframe/src/main/java/com/grapefruit/frontframe/FrontFrameApplication.java
+++ frontframe/src/main/java/com/grapefruit/frontframe/FrontFrameApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhangzhihuang
+ */
 @SpringBootApplication
 @MapperScan(basePackages = "com.grapefruit.frontframe.dao")
 public class FrontFrameApplication {
